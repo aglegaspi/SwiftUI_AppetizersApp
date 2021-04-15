16d9a9582373ff8fd79946c8a527127657b59256
aglegaspi <lex83nyc@gmail.com>
2021-04-15 09:12:36 -0400

    set up network manager function to make network call to return appetizer data from api

--- SwiftUI_Appetizers/NetworkManager.swift
+++ SwiftUI_Appetizers/NetworkManager.swift
@@ -9,16 +9,59 @@ import Foundation
 
 final class NetworkManager {
     
+    // singleton 
     static let shared = NetworkManager()
     
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     
-    static let appetizerURL = baseURL + "appetizers"
+    private let appetizerURL = baseURL + "appetizers"
     
     private init() {}
     
-    func getAppetizers(completed: @escaping (Result<[Appetizer], Error>) -> Void) {
-                        
+    // function completion handler that returns a result either a success and array of appetizerss or error of APError
+    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
+        
+        // check for good URL
+        guard let url = URL(string: appetizerURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        
+        // create network call to make request with good url it'll return to data, response, and error
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { (data, response, error) in
+            
+            // check for error i.e. not data signal
+            guard let _ = error else {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            // cast the response as a HTTPURLResponse and check status code make sure it's 200
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            // check for data
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            // decode data into the model
+            do {
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+                completed(.success(decodedResponse.request))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+            
+        }
+        
+        // fire off the network call
+        task.resume()
+        
     }
     
     
