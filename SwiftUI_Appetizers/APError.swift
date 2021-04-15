1533a928a28f91044389caef6eb184bb000ac28b
aglegaspi <lex83nyc@gmail.com>
2021-04-15 08:52:35 -0400

    set up custom errors

---
+++ SwiftUI_Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  SwiftUI_Appetizers
+//
+//  Created by Alex 6.1 on 4/15/21.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
