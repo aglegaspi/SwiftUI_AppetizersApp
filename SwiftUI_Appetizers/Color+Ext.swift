24e2fe5a144797b356921283fdc0c7ab97384c2f
aglegaspi <lex83nyc@gmail.com>
2021-04-19 16:54:30 -0400

    set up Loading View and refactor Color for reuse

---
+++ SwiftUI_Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  SwiftUI_Appetizers
+//
+//  Created by Alex 6.1 on 4/19/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
---
+++ SwiftUI_Appetizers/LoadingView.swift
@@ -0,0 +1,23 @@
+//
+//  LoadingView.swift
+//  SwiftUI_Appetizers
+//
+//  Created by Alex 6.1 on 4/19/21.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        let activityIndictorView = UIActivityIndicatorView(style: .large)
+        activityIndictorView.color = UIColor.brandPrimary
+        activityIndictorView.startAnimating()
+        return activityIndictorView
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+    
+    }
+    
+}
