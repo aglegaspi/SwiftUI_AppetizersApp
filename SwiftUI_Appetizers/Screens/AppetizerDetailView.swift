0963b563507d607e1e9b6256efe10f7847e54137
aglegaspi <lex83nyc@gmail.com>
2021-04-22 07:16:27 -0400

    update UI to Appetizer Detail View

---
+++ SwiftUI_Appetizers/Screens/AppetizerDetailView.swift
@@ -0,0 +1,100 @@
+//
+//  AppetizerDisplay.swift
+//  SwiftUI_Appetizers
+//
+//  Created by Alex 6.1 on 4/22/21.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    let appetizer: Appetizer
+    
+    var body: some View {
+        
+        VStack {
+            Image("food-placeholder")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 225, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            Text(appetizer.name)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            Spacer()
+            
+            Text(appetizer.description)
+                .multilineTextAlignment(.center)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                VStack {
+                    Text("Calories")
+                        .bold()
+                        .font(.caption)
+                    Text("\(appetizer.calories)")
+                        .foregroundColor(.secondary)
+                        .fontWeight(.semibold)
+                        .italic()
+                }
+                .padding()
+                
+                VStack {
+                    Text("Carbs")
+                        .bold()
+                        .font(.caption)
+                    Text("\(appetizer.carbs)")
+                        .foregroundColor(.secondary)
+                        .fontWeight(.semibold)
+                        .italic()
+                }
+                .padding()
+                
+                VStack {
+                    Text("Protein")
+                        .bold()
+                        .font(.caption)
+                    Text("\(appetizer.protein)")
+                        .foregroundColor(.secondary)
+                        .fontWeight(.semibold)
+                        .italic()
+                }
+                .padding()
+            }//VStack
+            
+            
+            Spacer()
+            
+            Button {
+                print("tapped")
+            } label: {
+                Text("\(appetizer.price, specifier: "%.2f") - Add To Order")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .frame(width: 260, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(10)
+            }
+            .foregroundColor(.white)
+            .padding(.bottom, 30)
+    
+            
+        }//VStack
+        .frame(width: 300, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+    }//View
+}//Struct
+
+struct AppetizerDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+    }
+}
