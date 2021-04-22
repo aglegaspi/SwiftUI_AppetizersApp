1068c32d5cf2820b6e877d0f03d7a5ac15908b19
aglegaspi <lex83nyc@gmail.com>
2021-04-22 06:53:11 -0400

    file organization

---
+++ SwiftUI_Appetizers/Screens/AppetizerDisplay.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizerDisplay.swift
+//  SwiftUI_Appetizers
+//
+//  Created by Alex 6.1 on 4/22/21.
+//
+
+import SwiftUI
+
+struct AppetizerDisplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppetizerDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerDisplay()
+    }
+}
