72072b4e400b82a82a590ffaf71e27ba47b83d72
aglegaspi <lex83nyc@gmail.com>
2021-04-25 15:34:21 -0700

    refactor dismiss button:

--- SwiftUI_Appetizers/Screens/AppetizerDetailView.swift
+++ SwiftUI_Appetizers/Screens/AppetizerDetailView.swift
@@ -84,17 +84,7 @@ struct AppetizerDetailView: View {
         .overlay(Button {
             isShowingDetail = false
         } label: {
-            ZStack {
-                Circle()
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(.white)
-                    .opacity(0.6)
-                
-                Image(systemName: "xmark")
-                    .imageScale(.small)
-                    .frame(width: 44, height: 44)
-                    .foregroundColor(.black)
-            }
+            XDismissButton()
         }, alignment: .topTrailing)
         
         
