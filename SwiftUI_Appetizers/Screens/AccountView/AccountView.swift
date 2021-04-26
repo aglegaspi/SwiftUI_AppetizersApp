f2d6a9efc293af1a901a0cc1e58e4d253cebeb4f
aglegaspi <lex83nyc@gmail.com>
2021-04-26 06:42:59 -0700

    add Sections and Fields to Form. create State properties

--- SwiftUI_Appetizers/Screens/AccountView/AccountView.swift
+++ SwiftUI_Appetizers/Screens/AccountView/AccountView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
             Form {
+                // Personal Information
                 Section(header: Text("Personal Information")) {
-                    Text("Account View!")
-                }
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $birthDate, displayedComponents: .date)
+                    
+                    Button {
+                        print("save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }//Section
+                
+                // Requests
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }//Section
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
 
             }//Form
             .navigationTitle("🗄 Account")
